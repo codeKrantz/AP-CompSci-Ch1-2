502dcf71bc94a98899923541e1a616535e79d8b8
Conrad Krantz <conradkrantz@gmail.com>
2024-01-24 21:12:15 -0600

    commenting

---
+++ Ch1.java
@@ -0,0 +1,28 @@
+// this us all the notes from chapter 1
+public class Ch1{
+
+public static void main(String[] args){
+    
+    // this is an example of a short commet
+
+    /*
+     This
+     Is
+     An
+     Example
+     Of
+     A
+     Long
+     Comment!
+     */
+
+
+
+
+
+
+}
+
+
+
+}
