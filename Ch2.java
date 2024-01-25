feac4c53e57bc1aa765270339e6d2b4d00558e3c
Conrad Krantz <conradkrantz@gmail.com>
2024-01-24 22:02:32 -0600

    Math class notes

---
+++ Ch2.java
@@ -0,0 +1,22 @@
+public class Ch2 {
+
+    public static void main(String[] args){
+        // Classes
+        // Objects that we create
+
+
+// The Math Class  
+// this class does not need to be imported
+System.out.println(Math.abs(-1)); // returns absolute value
+System.out.println(Math.pow(10, 2)); // returns the base raised to a power RETURNS A DOUBLE
+System.out.println(Math.sqrt(25)); // returns the square root AS A DOUBLE
+System.out.println(Math.random()); //A random double value between 0 and 1 / usually an irrational number
+System.out.println(Math.PI); // returns PI
+
+
+
+
+
+
+    }
+}
