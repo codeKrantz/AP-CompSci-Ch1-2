5310f55ce11b3c2a93240ca0d621b15022fbf915
Conrad Krantz <conradkrantz@gmail.com>
2024-01-24 22:13:48 -0600

    string class

--- Ch2.java
+++ Ch2.java
@@ -16,7 +16,33 @@ System.out.println(Math.PI); // returns PI
 
 
 
+/*
+ * String Class Methods:
+ *  Method
+Return
+length()
+An int equal to the length of the screen
+substring(int beginIndex)
+A String that is a subset of the original String, starting at beginIndex through the end
+substring(int beginIndex, int endIndex)
+A String that is a subset of the original String, starting at beginIndex up to but not including endIndex
+equals(Object anObject)
+A Boolean value - if the String is the same as anObject it returns true, otherwise it returns false
+compareTo(String anotherString)
+An int reflecting the status after comparing two strings lexicographically
+-Returns a negative integer if String comes before anotherString
+-Returns 0 if String is the same as anotherString
+-Returns a positive integer if String comes after anotherString
+indexOf(String aString)
+An int that is the index of the first occurrence of aString in the String. It will return -1 if aString does not appear in String
+
+ */
 
 
+
+
+
+
+ 
     }
 }
